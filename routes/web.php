0dd7fa7c3a7ca6d338dfa703c95d7d0c32582f8a
JessicaYolanda07 <jessiy.condoris@gmail.com>
2019-07-08 00:19:01 -0400

    configurando rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('programacionh', 'ProgramacionHController@materiatodo');
+
+Route::get('progamacionh', 'ProgramacionHController@index');
+
+
+Route::post('programacionh/', 'ProgramacionHController@crearh')->name('crearh');
+
+
+Route::get('formulario_crear_materia', 'FormCrearMateriaController@index');
+Route::get('editar/{id}', 'FormCrearMateriaController@editar')->name('editar');
+Route::put('editar/{id}', 'FormCrearMateriaController@update')->name('update');
+Route::delete('eliminar/{id}', 'FormCrearMateriaController@eliminar')->name('eliminar');
+Route::post('/', 'FormCrearMateriaController@crear')->name('crearm');
+
+Route::get('formulario_crear_horario', 'HorarioController@index1');
+Route::get('editarh/{id}', 'HorarioController@editar')->name('editar');
+Route::put('editarh/{id}', 'HorarioController@update')->name('update');
+Route::delete('eliminarh/{id}', 'HorarioController@eliminar')->name('eliminar');
+Route::post('horario/', 'HorarioController@crear')->name('crear');
